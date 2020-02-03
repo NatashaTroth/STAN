e846d7b6361ceaccaa10a57642ac7ec1a1360509
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-02-03 14:07:07 +0100

    add: routing

---
+++ client/src/pages/AddNew.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddNew() {
+    return (
+        <div>
+            <h2>New exam</h2>
+        </div>
+    );
+}
+
+export default AddNew;
---
+++ client/src/pages/Exams.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Exams() {
+    return (
+        <div>
+            <h2>Exams</h2>
+        </div>
+    );
+}
+
+export default Exams;
