e222bb5bad4e93f7592ccdc414629ea11bad6e2e
Natasha Troth <natashatroth@c78-104-170-55.fh-salzburg.ac.at>
2020-02-03 16:55:33 +0100

    refactor: app.js

--- server/app.js
+++ server/app.js
@@ -1,23 +1,16 @@
-//TODO: Extract middlewares into separate file
+//TODO: Extract middlewares into separate file??
 const express = require("express");
-// const graphqlHTTP = require('express-graphql')
-// const typeDefs = require("./schema/schema");
-// const { typeDefs } = require("./typedefs");
 const { typeDefs } = require("./typedefs");
-
 const { resolvers } = require("./resolvers");
 const mongoose = require("mongoose");
 const cors = require("cors");
-// import { graphqlExpress } from 'apollo-server-express';
-// const bodyParser = require('body-parser');
-// const {graphqlExpress} = require('apollo-server-express')
 const { ApolloServer, gql } = require("apollo-server-express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
 const { makeExecutableSchema } = require("apollo-server");
-
 const connectionString = "mongodb://localhost/MMP3";
+
+app.use(cors());
 
 mongoose
   .connect(connectionString, {
@@ -27,7 +20,6 @@ mongoose
   .then(() => console.log("connected to db"))
   .catch(e => console.error(e.message));
 
-// const server = new ApolloServer({ typeDefs, resolvers });
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers
@@ -39,26 +31,3 @@ server.applyMiddleware({ app });
 app.listen({ port: PORT }, () =>
   console.log(`🚀 Server ready at http://localhost:5000${server.graphqlPath}`)
 );
-
-// //connect to mlab database
-// const connectionString = "mongodb+srv://natasha:Bb6wHRMVqH8ODlVD@cluster0-abb8v.mongodb.net/test"
-
-// mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, () =>{
-//   //console.log("connected to db")
-// })
-// .then (() => console.log("connected to db"))
-// .catch((e) => console.error(e.message))
-
-// //so can use graphql on url /graphql
-// // app.use('/graphql', graphqlHTTP({
-// //   schema,
-// //   graphiql: true
-
-// // }))
-// app.use('/graphql',
-// bodyParser.json(),
-// graphqlExpress({ schema: myGraphQLSchema }));
-
-// app.listen(port, () => {
-//   console.log(`now listening to port ${port}`)
-// })
