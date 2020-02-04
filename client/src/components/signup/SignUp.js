ec6fb56af02e481653d3dafcdc8ddad10ddbecd3
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-02-04 16:57:02 +0100

    add: styling for login

--- client/src/components/signup/SignUp.js
+++ client/src/components/signup/SignUp.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { useQuery, useMutation } from "@apollo/react-hooks"
+import { useMutation } from "@apollo/react-hooks"
 import { SUCCESS_SIGNUP } from "../../graphQL/queries"
 import { ADD_USER_MUTATION } from "../../graphQL/mutations"
 // --------------------------------------------------------------
