72d9e3ac8c7e8d91d952dea905b9deb6371f207e
Natasha Troth <natashatroth@Natashas-MacBook-Pro.local>
2020-02-04 20:36:47 +0100

    fix: server babel error

--- server/app.js
+++ server/app.js
@@ -11,10 +11,10 @@ const PORT = process.env.PORT || 5000;
 const { makeExecutableSchema } = require("apollo-server");
 const connectionString = "mongodb://localhost/MMP3";
 
-const LocalStrategy = require("passport-local").Strategy;
+// const LocalStrategy = require("passport-local").Strategy;
 const session = require("express-session");
 const uuid = require("uuid/v4");
-import User from "./#testUsers";
+// import User from "./#testUsers";
 
 //TODO: for ease of implementation we hard-coded the secret, but taking it from environment variables would be the way to go for a production environment
 //you would want to set the cookie to secure mode so that it is only sent via https. You can use the cookie option for this: cookie: { secure: true }
