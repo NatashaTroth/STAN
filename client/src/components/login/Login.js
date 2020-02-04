4f85ccf7d7c439902560fe21fc46c5004eeb1062
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-02-04 19:37:05 +0100

    fix: height and width for signup and login

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -16,63 +16,58 @@ function Login() {
 
   return (
     <form onSubmit={handleSubmit} className="login__form box-content">
-      <div className="col-md-12 login__form__inner">
-        <Label
-          for="email"
-          text="E-Mail"
-          className="login__form__email-label"
-        ></Label>
-        <Input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="lucy@stan.io"
-          value={email}
-          maxLength={50}
-          className="login__form__email-input"
-          onChange={evt => setEmail(evt.target.value.slice(0, 50))}
-          required
-        />
-        <Label
-          for="password"
-          text="Pasword"
-          className="login__form__password-label"
-        ></Label>
-        <Input
-          type="password"
-          id="password"
-          name="password"
-          placeholder=""
-          value={password}
-          maxLength={30}
-          className="login__form__password-input"
-          onChange={evt => setPassword(evt.target.value.slice(0, 30))}
-          required
-        />
+      <Label
+        for="email"
+        text="E-Mail"
+        className="login__form__email-label"
+      ></Label>
+      <Input
+        type="email"
+        id="email"
+        name="email"
+        placeholder="lucy@stan.io"
+        value={email}
+        maxLength={50}
+        className="login__form__email-input"
+        onChange={evt => setEmail(evt.target.value.slice(0, 50))}
+        required
+      />
+      <Label
+        for="password"
+        text="Pasword"
+        className="login__form__password-label"
+      ></Label>
+      <Input
+        type="password"
+        id="password"
+        name="password"
+        placeholder=""
+        value={password}
+        maxLength={30}
+        className="login__form__password-input"
+        onChange={evt => setPassword(evt.target.value.slice(0, 30))}
+        required
+      />
 
-        <div className="login__form__buttons">
-          <div className="login__form__buttons__button-left">
-            <Button
-              variant="button"
-              text="Google Login"
-              className="stan-btn-secondary"
-            />
-          </div>
+      <div className="login__form__buttons">
+        <div className="login__form__buttons__button-left">
+          <Button
+            variant="button"
+            text="Google Login"
+            className="stan-btn-secondary"
+          />
+        </div>
 
-          <div className=" login__form__buttons__button-right">
-            <Button
-              variant="button"
-              text="Login"
-              className="stan-btn-primary"
-            />
-          </div>
+        <div className="login__form__buttons__button-right">
+          <Button variant="button" text="Login" className="stan-btn-primary" />
         </div>
-        <div className="login__form__redirect-signup">
-          <p className="login__form__redirect-signup__text">not registered?</p>{" "}
-          <a href="/sign-up" className="login__form__redirect-signup__link">
-            sign up
-          </a>
-        </div>
+      </div>
+
+      <div className="login__form__redirect-signup">
+        <p className="login__form__redirect-signup__text">not registered?</p>
+        <a href="/sign-up" className="login__form__redirect-signup__link">
+          sign up
+        </a>
       </div>
     </form>
   )
