537077d3bfc528bb1bde96e24fe52705cf1fbfa6
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-02-04 15:58:23 +0100

    add: box content to login form

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -15,56 +15,54 @@ function Login() {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="login__form">
-      <div className="box-content">
-        <Label
-          for="email"
-          text="E-Mail"
-          className="login__form__email-label"
-        ></Label>
-        <Input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="lucy@stan.io"
-          value={email}
-          maxLength={50}
-          className="login__form__email-input"
-          onChange={evt => setEmail(evt.target.value.slice(0, 50))}
-          required
-        />
-        <Label
-          for="password"
-          text="Pasword"
-          className="login__form__password-label"
-        ></Label>
-        <Input
-          type="password"
-          id="password"
-          name="password"
-          placeholder=""
-          value={password}
-          maxLength={30}
-          className="login__form__passwort-input"
-          onChange={evt => setPassword(evt.target.value.slice(0, 30))}
-          required
-        />
-        <div className="login__form__buttons">
-          <div className="col-md-6 login__button-left">
-            <Button variant="button" text="Google Login" />
-          </div>
+    <form onSubmit={handleSubmit} className="login__form box-content">
+      <Label
+        for="email"
+        text="E-Mail"
+        className="login__form__email-label"
+      ></Label>
+      <Input
+        type="email"
+        id="email"
+        name="email"
+        placeholder="lucy@stan.io"
+        value={email}
+        maxLength={50}
+        className="login__form__email-input"
+        onChange={evt => setEmail(evt.target.value.slice(0, 50))}
+        required
+      />
+      <Label
+        for="password"
+        text="Pasword"
+        className="login__form__password-label"
+      ></Label>
+      <Input
+        type="password"
+        id="password"
+        name="password"
+        placeholder=""
+        value={password}
+        maxLength={30}
+        className="login__form__passwort-input"
+        onChange={evt => setPassword(evt.target.value.slice(0, 30))}
+        required
+      />
+      <div className="login__form__buttons">
+        <div className="col-md-6 login__button-left">
+          <Button variant="button" text="Google Login" />
+        </div>
 
-          <div className="col-md-6 login__button-right">
-            <Button variant="button" text="Login" />
-          </div>
+        <div className="col-md-6 login__button-right">
+          <Button variant="button" text="Login" />
         </div>
-        <div className="col-md-12 login__form__redirect-signup">
-          <p className="login__form__redirect-signup__text">not registered?</p>{" "}
-          <a href="/sign-up" className="login__form__redirect-signup__link">
-            {" "}
-            sign up
-          </a>
-        </div>
+      </div>
+      <div className="col-md-12 login__form__redirect-signup">
+        <p className="login__form__redirect-signup__text">not registered?</p>{" "}
+        <a href="/sign-up" className="login__form__redirect-signup__link">
+          {" "}
+          sign up
+        </a>
       </div>
     </form>
   )
