ba99bb94942a376394459689623275ed0c2c964b
Daniela Kubesch <dkubesch.mmt-b2017@fh-salzburg.ac.at>
2020-02-05 14:50:21 +0100

    create & style: today-subject component

---
+++ client/src/components/today-subject/TodaySubject.js
@@ -0,0 +1,25 @@
+import React from "react"
+// --------------------------------------------------------------
+
+const TodaySubject = ({ subject, durationTime, id, onClick }) => {
+  // return ----------------
+  return (
+    <div className="today-subject" id={id} onClick={onClick}>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <h4 className="today-subject__heading">{subject}</h4>
+          </div>
+          <div className="col-md-12">
+            <div className="today-subject__duration">
+              <p className="today-subject__duration__text">Duration</p>
+              <p className="today-subject__duration__time">{durationTime}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TodaySubject
