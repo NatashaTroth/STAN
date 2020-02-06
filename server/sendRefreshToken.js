7228c0860dc4431eafccb342e95f9492bc11672e
Natasha Troth <natashatroth@c78-104-170-59.fh-salzburg.ac.at>
2020-02-06 18:39:22 +0100

    add: refresh token path

--- server/sendRefreshToken.js
+++ server/sendRefreshToken.js
@@ -5,7 +5,7 @@ export const sendRefreshToken = (res, token) => {
   //TODO: allow to expire cookie - so when logout - it deletes the cookie
   //TODO: rename refresh_token
   res.cookie("refresh_token", token, {
-    httpOnly: true
-    // path: "/refresh_token"
+    httpOnly: true,
+    path: "/refresh_token" //to only send request token when at refresh_token path
   });
 };
