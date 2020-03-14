b4190af2f298e46f89f897c7f003321edd9d1ce1
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-03-14 16:28:19 +0100

    add: calendar package

--- client/src/pages/calendar-page/CalendarPage.js
+++ client/src/pages/calendar-page/CalendarPage.js
@@ -1,11 +1,17 @@
 import React from "react"
+import FullCalendar from "@fullcalendar/react"
+import dayGridPlugin from "@fullcalendar/daygrid"
 
-function Calendar() {
-  return (
-    <div className="navigation__title">
-      <h2>Calendar</h2>
-    </div>
-  )
+class Calendar extends React.Component {
+  render() {
+    return (
+      <div className="calendar">
+        <div className="calendar__container container-fluid">
+          <FullCalendar defaultView="dayGridMonth" plugins={[dayGridPlugin]} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Calendar
