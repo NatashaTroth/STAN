05c16faf8d6f511b543c1e21afb82cb403099018
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-03-14 16:44:08 +0100

    add: box content and basic styling for calendar

--- client/src/pages/calendar-page/CalendarPage.js
+++ client/src/pages/calendar-page/CalendarPage.js
@@ -2,12 +2,21 @@ import React from "react"
 import FullCalendar from "@fullcalendar/react"
 import dayGridPlugin from "@fullcalendar/daygrid"
 
+// TODO: https://fullcalendar.io/docs/react
 class Calendar extends React.Component {
   render() {
     return (
       <div className="calendar">
         <div className="calendar__container container-fluid">
-          <FullCalendar defaultView="dayGridMonth" plugins={[dayGridPlugin]} />
+          <FullCalendar
+            classname=""
+            defaultView="dayGridMonth"
+            plugins={[dayGridPlugin]}
+            events={[
+              { title: "Test Event", date: "2020-03-14" },
+              { title: "Test Event", date: "2020-03-16" },
+            ]}
+          />
         </div>
       </div>
     )
