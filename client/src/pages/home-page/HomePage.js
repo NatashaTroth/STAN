8463c6afb06bc74a69441151763a9cc753544174
Daniela Kubesch <dkubesch.mmt-b2017@fh-salzburg.ac.at>
2020-03-15 21:39:58 +0100

    refactor: home page

--- client/src/pages/home-page/HomePage.js
+++ client/src/pages/home-page/HomePage.js
@@ -1,46 +1,48 @@
 import React from "react"
+// --------------------------------------------------------------
 
-// sub-components
-import Paragraph from "../../components/paragraph/Paragraph"
+// components ----------------
 import SubHeading from "../../components/sub-heading/SubHeading"
 import Listing from "../../components/listing/Listing"
-import Button from "../../components/button/Button"
 
 function Home() {
+  // return ----------------
   return (
     <div className="home">
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-12">
-            <div className="col-md-8">
-              <h2 className="home__heading">Organize your Study Plan</h2>
-              <SubHeading className="sub-heading" text="Easy . Fast . Online" />
+          <div className="col-md-1"></div>
+          <div className="col-md-10">
+            <h2 className="home__heading">
+              Organize your <br></br>Study Plan
+            </h2>
+            <SubHeading className="sub-heading" text="Easy . Fast . Online" />
 
-              <Paragraph
-                className="home__text"
-                text="Students like you can add and organize their learning material for exams with Stan! Stan supports you in dividing up your tasks for each subject. Deadlines, as well as calculated learning chunks, are visualized so you can keep track of your progress and counter procrastination."
+            <p className="home__text">
+              Students like you can add and organize their learning material for
+              exams with Stan! Stan supports you in dividing up your tasks for
+              each subject. Deadlines, as well as calculated learning chunks,
+              are visualized so you can keep track of your progress and counter
+              procrastination.
+            </p>
+
+            <div className="home__list">
+              <Listing
+                className="list-style"
+                text="increase your learning motivation"
               />
-
-              <div className="home__list">
-                <Listing
-                  className="list-item"
-                  text="increase your learning motivation"
-                />
-                <Listing
-                  className="list-item"
-                  text="decrease your procrastination"
-                />
-              </div>
-
-              <div className="home__btn">
-                <Button
-                  className="stan-btn-double"
-                  variant="button"
-                  text="Start now"
-                />
-              </div>
+              <Listing
+                className="list-style"
+                text="decrease your procrastination"
+              />
             </div>
-            <div className="col-md-6"></div>
+
+            <div className="home__btn">
+              <a href="/sign-up" className="stan-btn-double">
+                Start now
+              </a>
+            </div>
+            <div className="col-md-1"></div>
           </div>
         </div>
       </div>
