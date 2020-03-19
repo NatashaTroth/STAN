1806164927f16858fa0a216d58b88eea49c11a36
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-03-19 15:09:47 +0100

    add: current exams container blocks

--- client/src/pages/exams-page/ExamsPage.js
+++ client/src/pages/exams-page/ExamsPage.js
@@ -4,8 +4,39 @@ import React from "react"
 function Exams() {
   // return ----------------
   return (
-    <div className="navigation__title">
-      <h2>Exams</h2>
+    <div className="exams">
+      <div className="container-fluid">
+        <div className="exams__headline">
+          <h2>Current Exams</h2>
+        </div>
+
+        <div className="exams__container">
+          <div className="exams__container__subject box-content">
+            <div className="exams__container__subject--headline">
+              <h4>Backend Development</h4>
+            </div>
+            <div className="exams__container__subject--current-status">
+              <p>completed 20%</p>
+            </div>
+          </div>
+          <div className="exams__container__subject box-content">
+            <div className="exams__container__subject--headline">
+              <h4>Math & Statistics</h4>
+            </div>
+            <div className="exams__container__subject--current-status">
+              <p>completed 98%</p>
+            </div>
+          </div>
+          <div className="exams__container__subject box-content">
+            <div className="exams__container__subject--headline">
+              <h4>Computer Networks</h4>
+            </div>
+            <div className="exams__container__subject--current-status">
+              <p>completed 67%</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
