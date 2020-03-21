0c6b1e222c0718d60ccb4ac4b74831abe19ba8f3
Natasha Troth <ntroth.mmt-b2017@fh.salzburg.ac.at>
2020-03-21 21:14:30 +0100

    add: jest testing backend

---
+++ server/tests/integration/integration.test.js
@@ -0,0 +1,14 @@
+//https://www.apollographql.com/docs/apollo-server/testing/testing/
+const { createTestClient } = require("apollo-server-testing");
+
+const { query, mutate } = createTestClient(server);
+
+query({
+  query: GET_USER,
+  variables: { id: 1 }
+});
+
+mutate({
+  mutation: UPDATE_USER,
+  variables: { id: 1, email: "nancy@foo.co" }
+});
