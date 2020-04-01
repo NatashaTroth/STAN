69e92a211b0ad1680af3593b3acd489b996dc886
Daniela Kubesch <dkubesch.mmt-b2017@fh-salzburg.ac.at>
2020-04-01 17:12:46 +0200

    add: buttons to dashboard today component

--- client/src/components/today/Today.js
+++ client/src/components/today/Today.js
@@ -1,14 +1,44 @@
 import React from "react"
 import { useQuery } from "@apollo/react-hooks"
 import { GET_USERS_QUERY } from "../../graphQL/queries"
+import { useForm } from "react-hook-form"
 // --------------------------------------------------------------
 
 // components ----------------
-import TodaySubject from "../../components/today-subject/TodaySubject"
+import Button from "../../components/button/Button"
+import Label from "../../components/label/Label"
+import Input from "../../components/input/Input"
 
 function Today() {
+  // form specific ----------------
+  const { register, errors, handleSubmit } = useForm()
+
+  const onSubmit = async formData => {
+    // try {
+    //   console.log(JSON.stringify(formData.exam_date))
+    //   const resp = await addChunkPages({
+    //     variables: {
+    //       numberPages: parseInt(formData.page_amount_studied),
+    //       userId: data.currentUser.id,
+    //     },
+    //     refetchQueries: [{ query: GET_EXAMS_QUERY }],
+    //   })
+    //   if (resp && resp.data && resp.data.addChunkPages) {
+    //     // TODO: remove block from dashboard
+    //   } else {
+    //     // displays server error (backend)
+    //     throw new Error("Your input could not be saved")
+    //   }
+    // } catch (err) {
+    //   console.error(err.message)
+    // }
+  }
+
   // query ----------------
   const { loading, error } = useQuery(GET_USERS_QUERY)
+
+  // mutation ----------------
+  //   const [addChunkPages, { mutationData }] = useMutation(ADD_EXAM_MUTATION)
 
   // error handling ----------------
   if (loading) return <p>Loading...</p>
@@ -69,6 +99,71 @@ function Today() {
                       Total pages
                     </p>
                     <p className="today__container__content__text">280 / 890</p>
+                  </div>
+                </div>
+                {/* days left */}
+                {/* chunks left */}
+                {/* buttons */}
+                <div className="today__container__buttons">
+                  {/* open notes or link */}
+                  {/* TODO: add link to exam */}
+                  <a href="/exams" className="today__container__buttons__open">
+                    open notes
+                  </a>
+
+                  <div className="today__container__buttons__submit">
+                    {/* pages done */}
+                    <form
+                      // onSubmit={handleSubmit}
+                      onSubmit={handleSubmit(onSubmit)}
+                      id="study-chunk"
+                      className="today__container__buttons__submit__form"
+                    >
+                      <div className="today__container__buttons__submit__form__elements">
+                        <Label
+                          for="page-amount-studied"
+                          text="studied up to page:"
+                          className="today__container__buttons__submit__form__elements__label"
+                        ></Label>
+                        <Input
+                          className="today__container__buttons__submit__form__elements__input"
+                          type="number"
+                          min="0"
+                          id="page-amount-studied"
+                          label="page_amount_studied"
+                          placeholder="120"
+                          ref={register({
+                            min: 1,
+                            max: 10000,
+                          })}
+                          // TODO: maximum is page amount
+                          // TODO: placeholder is current page
+                        />
+                        {errors.page_amount_studied &&
+                          errors.page_amount_studied.type === "max" && (
+                            <span className="error">The maximum is 10.000</span>
+                          )}
+                        {errors.page_amount_studied &&
+                          errors.page_amount_studied.type === "min" && (
+                            <span className="error">
+                              Only positive numbers are allowed
+                            </span>
+                          )}
+                      </div>
+                      <Button
+                        className="today__container__buttons__submit__form__btn stan-btn-secondary"
+                        variant="button"
+                        text="save"
+                      />
+                    </form>
+                    {/* all done */}
+                    <div className="today__container__buttons__submit__all-done">
+                      <Button
+                        className="today__container__buttons__submit__all-done__btn stan-btn-primary"
+                        variant="button"
+                        text="goal studied"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
