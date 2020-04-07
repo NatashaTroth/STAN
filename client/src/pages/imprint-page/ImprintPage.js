fbfa31f20d0d634b77a082ea57a44ba85b965076
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-04-07 21:42:22 +0200

    refactor: basic styling for imprint

--- client/src/pages/imprint-page/ImprintPage.js
+++ client/src/pages/imprint-page/ImprintPage.js
@@ -1,50 +1,63 @@
 import React from "react"
 // --------------------------------------------------------------
 
+// image ----------------
+// import Stan from "../../images/mascots/imprintStan"
+
 function Imprint() {
   // return ----------------
   return (
     <div className="imprint-page">
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-12">
-            <h2 className="imprint-page__heading">Imprint</h2>
-          </div>
-          <div className="col-md-6">
-            <p className="imprint-page__text">
-              This is a MultiMedia Project 3 by Natasha, Tra and Daniela.
-            </p>
-            <p className="imprint-page__text">
-              Fachhochschule Salzburg GmbH
-              <br></br>
-              Urstein Süd 1<br></br>
-              A-5412
-              <br></br>
-              Puch/Salzburg Österreich
-            </p>
-            <div className="imprint-page__contact">
-              <p className="imprint-page__contact__container">
+          <div className="col-md-1"></div>
+          <div className="col-m-10">
+            <div className="imprint__headline">
+              <h2>Imprint</h2>
+            </div>
+
+            <div className="imprint__content">
+              <p>This is a MultiMedia Project 3 by Natasha, Tra and Daniela.</p>
+            </div>
+
+            <div className="imprint__content">
+              <p>
+                Fachhochschule Salzburg GmbH
+                <br></br>
+                Urstein Süd 1<br></br>
+                A-5412
+                <br></br>
+                Puch/Salzburg Österreich
+              </p>
+            </div>
+
+            <div className="imprint__content">
+              <p className="imprint__content--contact">
                 <strong>T </strong>
-                <a
-                  href="tel:+435022110"
-                  className="imprint-page__contact__container__link"
-                >
+                <a href="tel:+435022110" className="imprint__content--link">
                   +43 50-2211-0
                 </a>
               </p>
-              <p className="imprint-page__contact__container">
+              <p className="imprint__content--contact">
                 <strong>Mail </strong>
                 <a
                   href="mailto:medien@fh-salzburg.ac.at"
-                  className="imprint-page__contact__container__link"
+                  className="imprint__content--link"
                 >
                   medien@fh-salzburg.ac.at
                 </a>
               </p>
             </div>
-            <p className="imprint-page__text">© 2020 FH Salzburg</p>
+
+            <div className="imprint__content">
+              <p>© 2020 FH Salzburg</p>
+
+              {/* <img src={Stan} alt="a mascot is holding up a letter" /> */}
+
+              <span className="imprint__content--line"></span>
+            </div>
           </div>
-          <div className="col-md-6"></div>
+          <div className="col-md-1"></div>
         </div>
       </div>
     </div>
