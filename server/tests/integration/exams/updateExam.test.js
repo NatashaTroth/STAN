66d3394e90c0d0269db551f1218947d0f77172ae
Natasha Troth <ntroth.mmt-b2017@fh.salzburg.ac.at>
2020-04-13 22:53:52 +0200

    refactor: linting

--- server/tests/integration/exams/updateExam.test.js
+++ server/tests/integration/exams/updateExam.test.js
@@ -10,7 +10,7 @@ import {
 } from "../setup";
 import { Exam } from "../../../models";
 
-import { ADD_EXAM_MUTATION, UPDATE_EXAM_MUTATION } from "../../mutations.js";
+import { UPDATE_EXAM_MUTATION } from "../../mutations.js";
 
 // import { createTestClient } from "apollo-server-integration-testing";
 
