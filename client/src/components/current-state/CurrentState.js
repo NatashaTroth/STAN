c40df1a002251e8d48b18258142e2a6e285a4bf6
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-04-21 10:13:15 +0200

    prepate: query for current state

--- client/src/components/current-state/CurrentState.js
+++ client/src/components/current-state/CurrentState.js
@@ -7,23 +7,39 @@ import {
   useCurrentUserValue,
 } from "../../components/STAN/STAN"
 
+// query ----------------
+// import { GET_CURRENT_USER_STATE } from "../../graphQL/queries"
+
 // components ----------------
 import Image from "../../components/image/Image"
 
 // motivational sayings ----------------
-import data from "./json/motivational-sayings.json"
+import motivationalSayings from "./json/motivational-sayings.json"
 
 // libraries ----------------
 import { Carousel } from "react-responsive-carousel"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { useQuery } from "@apollo/react-hooks"
+import QueryError from "../error/Error"
 
 const random = Math.floor(Math.random() * 3)
 
 const CurrentState = () => {
-  let mood = "very stressed"
+  // query ----------------
+  // const { data, loading, error } = useQuery(GET_CURRENT_USER_STATE)
+
+  // variables ----------------
+  let mood = "okay"
   let motivation
 
-  data.forEach(element => {
+  // error handling ----------------
+  // if (loading) return <p>loading...</p>
+  // if (error) return <QueryError errorMessage={error.message} />
+  // if (data) {
+  //   mood = data
+  // }
+
+  motivationalSayings.forEach(element => {
     if (mood === element.mood) {
       if (element.id === random) {
         motivation = element.motivation
