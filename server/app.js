06fe2ecf3572503fc8dac3b3c8f64c4343b8d3a1
Natasha Troth <ntroth.mmt-b2017@fh.salzburg.ac.at>
2020-04-28 21:51:25 +0200

    add: compression of static frontend

--- server/app.js
+++ server/app.js
@@ -11,12 +11,14 @@ import { isAuth } from "./helpers/is-auth";
 import cookieParser from "cookie-parser";
 import { handleRefreshToken } from "./helpers/authenticationTokens";
 import path from "path";
+var compress = require("compression");
 
 // import { stanImage } from "./stanBackend.svg";
 //TODO: CACHING APOLLO
 const connectionString = process.env.MONGODB_URI || "mongodb://localhost/MMP3";
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(compress());
 //TODO: EXTRACT MONGODB CONNECTIONS
 
 mongoose
@@ -52,22 +54,22 @@ app.post("/refresh_token", async (req, res) => {
 });
 
 //TODO: remove /graphql when deployed
-if (process.env.NODE_ENV === "production") {
-  //TODO: CHANGE TO /graphql
-  app.use("/backend", express.static(__dirname + "/backend"));
-  app.get("/backend", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "backend", "index.html"));
-  });
-  app.use(express.static("public"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "public", "index.html"));
-  });
-} else {
-  app.use("/backend", express.static(__dirname + "/backend"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "backend", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//TODO: CHANGE TO /graphql
+app.use("/backend", express.static(__dirname + "/backend"));
+app.get("/backend", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "backend", "index.html"));
+});
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+// } else {
+//   app.use("/backend", express.static(__dirname + "/backend"));
+//   app.get("/", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "backend", "index.html"));
+//   });
+// }
 
 const apolloServer = new ApolloServer({
   schema,
