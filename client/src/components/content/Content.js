fbdbf8520c29753ff56d9929dc0aecb3633caab3
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-04-29 11:16:04 +0200

    add: lazy loading in content.js

--- client/src/components/content/Content.js
+++ client/src/components/content/Content.js
@@ -1,43 +1,51 @@
-import React from "react"
+import React, { lazy, Suspense } from "react"
 import { Switch, Route, withRouter } from "react-router-dom"
 // --------------------------------------------------------------
 
 // pages component ----------------
-import AddNew from "../../pages/add-new-page/AddNewPage"
-import Calendar from "../../pages/calendar-page/CalendarPage"
-import Exams from "../../pages/exams-page/ExamsPage"
-import NoMatch404 from "../../pages/no-match-page/404Page"
-import UserAccount from "../../pages/user-account-page/UserAccountPage"
-import Imprint from "../../pages/imprint-page/ImprintPage"
-import DataPolicy from "../../pages/data-policy-page/DataPolicyPage"
-import Login from "../../pages/login-page/LoginPage"
-import SignUp from "../../pages/sign-up-page/SignUpPage"
-import Home from "../../pages/home-page/HomePage"
-import About from "../../pages/about-page/AboutPage"
-import LoginPopUp from "../../components/login-popup/LoginPopUp"
-import ExamsDetails from "../../components/current-exam/ExamDetails"
+const AddNew = lazy(() => import("../../pages/add-new-page/AddNewPage"))
+const Calendar = lazy(() => import("../../pages/calendar-page/CalendarPage"))
+const Exams = lazy(() => import("../../pages/exams-page/ExamsPage"))
+const NoMatch404 = lazy(() => import("../../pages/no-match-page/404Page"))
+const UserAccount = lazy(() =>
+  import("../../pages/user-account-page/UserAccountPage")
+)
+const Imprint = lazy(() => import("../../pages/imprint-page/ImprintPage"))
+const DataPolicy = lazy(() =>
+  import("../../pages/data-policy-page/DataPolicyPage")
+)
+const Login = lazy(() => import("../../pages/login-page/LoginPage"))
+const SignUp = lazy(() => import("../../pages/sign-up-page/SignUpPage"))
+const Home = lazy(() => import("../../pages/home-page/HomePage"))
+const About = lazy(() => import("../../pages/about-page/AboutPage"))
+const LoginPopUp = lazy(() => import("../../components/login-popup/LoginPopUp"))
+const ExamsDetails = lazy(() =>
+  import("../../components/current-exam/ExamDetails")
+)
 
 const Content = ({ location }) => {
   // return ----------------
   return (
     <main className="content">
-      <Switch location={location}>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/sign-up" component={SignUp} />
-        <Route exact path="/popup" component={LoginPopUp} />
+      <Suspense fallback={<h1>Still Loading…</h1>}>
+        <Switch location={location}>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/sign-up" component={SignUp} />
+          <Route exact path="/popup" component={LoginPopUp} />
 
-        <Route exact path="/add-new" component={AddNew} />
-        <Route exact path="/calendar" component={Calendar} />
-        <Route exact path="/exams" component={Exams} />
-        <Route exact path="/exams/:subject" component={ExamsDetails} />
-        <Route exact path="/profile" component={UserAccount} />
+          <Route exact path="/add-new" component={AddNew} />
+          <Route exact path="/calendar" component={Calendar} />
+          <Route exact path="/exams" component={Exams} />
+          <Route exact path="/exams/:subject" component={ExamsDetails} />
+          <Route exact path="/profile" component={UserAccount} />
 
-        <Route exact path="/imprint" component={Imprint} />
-        <Route exact path="/data-policy" component={DataPolicy} />
-        <Route path="*" component={NoMatch404} />
-      </Switch>
+          <Route exact path="/imprint" component={Imprint} />
+          <Route exact path="/data-policy" component={DataPolicy} />
+          <Route path="*" component={NoMatch404} />
+        </Switch>
+      </Suspense>
     </main>
   )
 }
