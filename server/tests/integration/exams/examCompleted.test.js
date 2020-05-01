ef889a430fbd32fd5f6e1ab95765611131578896
Natasha Troth <ntroth.mmt-b2017@fh.salzburg.ac.at>
2020-05-01 18:46:08 +0200

    test: examCompleted

---
+++ server/tests/integration/exams/examCompleted.test.js
@@ -0,0 +1,77 @@
+//https://www.apollographql.com/docs/apollo-server/testing/testing/
+//https://mongoosejs.com/docs/jest.html
+import { createTestClient } from "apollo-server-testing";
+import {
+  setupApolloServer,
+  setupDb,
+  addTestExam,
+  // clearDatabase,
+  teardown
+} from "../setup";
+import { Exam, TodaysChunkCache } from "../../../models";
+
+import { EXAM_COMPLETED_MUTATION } from "../../mutations.js";
+import { GET_TODAYS_CHUNKS } from "../../queries.js";
+
+// import { createTestClient } from "apollo-server-integration-testing";
+
+describe("Test user resolver regex", () => {
+  let server;
+  let mutate;
+  let query;
+  beforeAll(async () => {
+    await setupDb();
+    server = await setupApolloServer({ isAuth: true, userId: "samanthasId" });
+    let client = createTestClient(server);
+    mutate = client.mutate;
+    query = client.query;
+  });
+
+  // afterEach(async () => {
+  //   await clearDatabase();
+  // });
+
+  afterAll(async () => {
+    await teardown();
+  });
+
+  it("should make sure exam and chunk cache are completed when use examCompleted mutation", async () => {
+    const testExam = await addTestExam({ subject: "Biology" });
+    expect(await Exam.countDocuments({ userId: "samanthasId" })).toBe(1);
+
+    const respTodaysChunks = await query({
+      query: GET_TODAYS_CHUNKS
+    });
+    expect(respTodaysChunks.data.todaysChunks).toBeTruthy();
+    expect(respTodaysChunks.data.todaysChunks.length).toBe(1);
+    expect(
+      await TodaysChunkCache.countDocuments({ userId: "samanthasId" })
+    ).toBe(1);
+
+    const resp = await mutate({
+      query: EXAM_COMPLETED_MUTATION,
+      variables: {
+        id: testExam._id.toString()
+      }
+    });
+    expect(await Exam.countDocuments({ userId: "samanthasId" })).toBe(1);
+    expect(
+      await TodaysChunkCache.countDocuments({ userId: "samanthasId" })
+    ).toBe(1);
+
+    const exam = await Exam.findOne({ userId: "samanthasId" });
+    const todaysChunkCache = await TodaysChunkCache.findOne({
+      userId: "samanthasId"
+    });
+
+    expect(exam.completed).toBeTruthy();
+    expect(todaysChunkCache.completed).toBeTruthy();
+
+    // const exam = await query({
+    //   query: GET_EXAM,
+    //   variables: {
+    //     id:testExam._id.toString()
+    //   }
+    // });
+  });
+});
