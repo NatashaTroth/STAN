451d84fdb1afbb331360ba977bb8f84fcc68b638
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-05-07 22:10:20 +0200

    add: carousel bootstrap and remove old carousel library

--- client/src/components/current-state/CurrentState.js
+++ client/src/components/current-state/CurrentState.js
@@ -1,30 +1,31 @@
 import React from "react"
+import { useQuery } from "@apollo/react-hooks"
 // --------------------------------------------------------------
 
 // context ----------------
-import { CurrentUserContext } from "../../components/STAN/STAN"
+import { useCurrentUserValue } from "../../components/STAN/STAN"
 
 // query ----------------
 import { GET_TODAYS_CHUNKS_PROGRESS } from "../../graphQL/queries"
 
-// sub-components ----------------
+// components ----------------
 import Image from "../../components/image/Image"
 import Loading from "../../components/loading/Loading"
+import QueryError from "../error/Error"
 import { currentMood } from "../../pages/user-account-page/UserAccountPage"
 
 // motivational sayings ----------------
 import motivationalSayings from "./json/motivational-sayings.json"
 
 // libraries ----------------
-import { Carousel } from "react-responsive-carousel"
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import { useQuery } from "@apollo/react-hooks"
-import QueryError from "../error/Error"
+import Carousel from "react-bootstrap/Carousel"
 
 // random number ----------------
 const random = Math.floor(Math.random() * 3)
 
 const CurrentState = () => {
+  const currentUser = useCurrentUserValue()
+
   // query ----------------
   const { data, loading, error } = useQuery(GET_TODAYS_CHUNKS_PROGRESS)
 
@@ -50,47 +51,33 @@ const CurrentState = () => {
   // return ----------------
   return (
     <div className="current-state">
-      <Carousel
-        showStatus={false}
-        showThumbs={false}
-        infiniteLoop={true}
-        showIndicators={false}
-        autoPlay={true}
-        showArrows={false}
-        interval={30000}
-      >
-        <CurrentUserContext.Consumer>
-          {currentUser => (
-            <Image
-              path={require(`../../images/mascots/${
-                currentUser.mascot
-              }-${mood.replace(/ /g, "")}-0.svg`)}
-              text=""
-            />
-          )}
-        </CurrentUserContext.Consumer>
+      <Carousel indicators={false} controls={false} interval={1000}>
+        <Carousel.Item>
+          <Image
+            path={require(`../../images/mascots/${
+              currentUser.mascot
+            }-${mood.replace(/ /g, "")}-0.svg`)}
+            text=""
+          />
+        </Carousel.Item>
 
-        <CurrentUserContext.Consumer>
-          {currentUser => (
-            <Image
-              path={require(`../../images/mascots/${
-                currentUser.mascot
-              }-${mood.replace(/ /g, "")}-1.svg`)}
-              text=""
-            />
-          )}
-        </CurrentUserContext.Consumer>
+        <Carousel.Item>
+          <Image
+            path={require(`../../images/mascots/${
+              currentUser.mascot
+            }-${mood.replace(/ /g, "")}-1.svg`)}
+            text=""
+          />
+        </Carousel.Item>
 
-        <CurrentUserContext.Consumer>
-          {currentUser => (
-            <Image
-              path={require(`../../images/mascots/${
-                currentUser.mascot
-              }-${mood.replace(/ /g, "")}-2.svg`)}
-              text=""
-            />
-          )}
-        </CurrentUserContext.Consumer>
+        <Carousel.Item>
+          <Image
+            path={require(`../../images/mascots/${
+              currentUser.mascot
+            }-${mood.replace(/ /g, "")}-2.svg`)}
+            text=""
+          />
+        </Carousel.Item>
       </Carousel>
 
       <div className="current-state__motivation">
