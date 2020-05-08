e8bb0daf9ece5be8ad4a43ff21c58311bc805729
Daniela Kubesch <dkubesch.mmt-b2017@fh-salzburg.ac.at>
2020-05-08 13:06:01 +0200

    refactor: format date in date picker with moment.js

--- client/src/components/datepicker/DatePicker.js
+++ client/src/components/datepicker/DatePicker.js
@@ -1,21 +1,8 @@
 import React from "react"
 import DayPickerInput from "react-day-picker/DayPickerInput"
 import "react-day-picker/lib/style.css"
-import dateFnsFormat from "date-fns/format"
-import dateFnsParse from "date-fns/parse"
-import { DateUtils } from "react-day-picker"
-
-function parseDate(str, format, locale) {
-  const parsed = dateFnsParse(str, format, new Date(), { locale })
-  if (DateUtils.isDate(parsed)) {
-    return parsed
-  }
-  return undefined
-}
-
-function formatDate(date, format, locale) {
-  return dateFnsFormat(date, format, { locale })
-}
+import { formatDate, parseDate } from "react-day-picker/moment"
+// --------------------------------------------------------------
 
 export default class DatePicker extends React.Component {
   constructor(props) {
@@ -23,26 +10,22 @@ export default class DatePicker extends React.Component {
     this.handleDayChange = this.handleDayChange.bind(this)
     this.state = {
       selectedDay: undefined,
-      isEmpty: true,
-      isDisabled: false,
     }
   }
 
-  handleDayChange(selectedDay, modifiers, dayPickerInput) {
-    const input = dayPickerInput.getInput()
+  handleDayChange(selectedDay) {
     this.setState({
       selectedDay,
-      isEmpty: !input.value.trim(),
-      isDisabled: modifiers.disabled === true,
     })
     this.props.onDaySelected(selectedDay)
   }
 
   render() {
-    const { selectedDay, isDisabled, isEmpty } = this.state
-    const FORMAT = "dd.MM.yyyy"
+    const { selectedDay } = this.state
+    const FORMAT = "DD.MM.yyyy"
     const today = new Date()
 
+    // return ----------------
     return (
       <DayPickerInput
         value={selectedDay}
