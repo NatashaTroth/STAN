0eec6e6f32d10e0b651b0f86b0795b1cd050166a
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-05-08 19:51:25 +0200

    add: apollo client in exams page

--- client/src/pages/exams-page/ExamsPage.js
+++ client/src/pages/exams-page/ExamsPage.js
@@ -2,11 +2,8 @@ import React, { useState } from "react"
 import { Redirect, Link, useRouteMatch } from "react-router-dom"
 // --------------------------------------------------------------
 
-// context ----------------
-import { useCurrentUserValue } from "../../components/STAN/STAN"
-
 // queries ----------------
-import { GET_EXAMS_QUERY } from "../../graphQL/queries"
+import { GET_EXAMS_QUERY, CURRENT_USER } from "../../graphQL/queries"
 import { useQuery } from "@apollo/react-hooks"
 
 // components ----------------
@@ -14,13 +11,16 @@ import Exam from "../../components/exams/Exam"
 import QueryError from "../../components/error/Error"
 import Loading from "../../components/loading/Loading"
 
+// cache ----------------
+import { client } from "../../apolloClient"
+
 const Exams = () => {
   // router ----------------
   let { url } = useRouteMatch()
 
   // state & queries ----------------
   const [isArchiveOpen, setArchiveExams] = useState(false)
-  const { data, loading, error } = useQuery(GET_EXAMS_QUERY)
+  const { loading, error } = useQuery(GET_EXAMS_QUERY)
 
   // variables ----------------
   let currentExams, archiveExams
@@ -28,34 +28,36 @@ const Exams = () => {
   let archiveExamsList = []
 
   // redirects ----------------
-  const currentUser = useCurrentUserValue()
-  if (currentUser === undefined) {
+  const currentUser = client.readQuery({ query: CURRENT_USER }).currentUser
+  if (currentUser === null) {
     return <Redirect to="/login" />
   }
 
   if (loading) return <Loading />
   if (error) return <QueryError errorMessage={error.message} />
-  if (data && data.exams) {
-    data.exams.forEach(exam => {
-      if (!exam.completed) {
-        currentExamsList.push({
-          id: exam.id,
-          subject: exam.subject,
-          numberPages: exam.numberPages,
-          currentPage: exam.currentPage,
-          timesRepeat: exam.timesRepeat,
-        })
-      } else {
-        archiveExamsList.push({
-          id: exam.id,
-          subject: exam.subject,
-          numberPages: exam.numberPages,
-          currentPage: exam.currentPage,
-          timesRepeat: exam.timesRepeat,
-        })
-      }
-    })
-  }
+
+  // run queries in cache ----------------
+  const data = client.readQuery({ query: GET_EXAMS_QUERY }).exams
+
+  data.forEach(exam => {
+    if (!exam.completed) {
+      currentExamsList.push({
+        id: exam.id,
+        subject: exam.subject,
+        numberPages: exam.numberPages,
+        currentPage: exam.currentPage,
+        timesRepeat: exam.timesRepeat,
+      })
+    } else {
+      archiveExamsList.push({
+        id: exam.id,
+        subject: exam.subject,
+        numberPages: exam.numberPages,
+        currentPage: exam.currentPage,
+        timesRepeat: exam.timesRepeat,
+      })
+    }
+  })
 
   // functions ----------------
   currentExams = currentExamsList.map(function(exam) {
