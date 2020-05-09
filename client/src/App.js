6c62e5ce8eaca4dd31e4c69e3fd66269b8e25b84
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-05-09 10:52:21 +0200

    add: comments in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from "react"
-import "./App.scss"
-// import ApolloClient from "apollo-boost"
-import { ApolloProvider } from "@apollo/react-hooks" //inserts received data into our app
 import { setAccessToken } from "./accessToken"
 import { BrowserRouter as Router, Redirect } from "react-router-dom"
+import "./App.scss"
+// --------------------------------------------------------------
+
+// inserts received data into our app ----------------
+import { ApolloProvider } from "@apollo/react-hooks"
 
 import { client } from "./apolloClient"
 
-// components
+// components ----------------
 import STAN from "./components/STAN/STAN"
 import Loading from "./components/loading/Loading"
 
+// preloader animation ----------------
 import { Cube } from "react-preloaders"
 
+// redirect function ----------------
 const RedirectPopup = () => {
   return (
     <Router>
@@ -25,7 +29,7 @@ const RedirectPopup = () => {
 const App = () => {
   const [loading, setLoading] = useState(true)
 
-  //TODO-LOGOUT-ALL-TABS
+  // TODO: LOGOUT-ALL-TABS
   window.addEventListener("storage", e => {
     if (e.key === "logout-event") {
       localStorage.removeItem("logout-event")
@@ -36,10 +40,6 @@ const App = () => {
   })
 
   useEffect(() => {
-    // TODO: if save accesstoken in store, use the if - but if only save in memory delete
-    //if (!getAccessToken()) {
-    // fetch("http://localhost:5000/refresh_token", {
-    // console.log("here:" + process.env.DOMAIN_REFRESH_TOKEN)
     fetch("/refresh_token", {
       method: "POST",
       credentials: "include",
@@ -52,7 +52,6 @@ const App = () => {
       .catch(err => {
         console.error(err)
       })
-    // }
 
     //In case a second tab wasn't open, to make sure it is deleted
     // localStorage.removeItem("logout-event")
--- client/src/components/STAN/STAN.js
+++ client/src/components/STAN/STAN.js
@@ -1,13 +1,10 @@
-import React, { createContext, useContext, useState } from "react"
+import React, { useState } from "react"
 import { BrowserRouter as Router, Link, NavLink } from "react-router-dom"
 // --------------------------------------------------------------
 
 // mutation & queries ----------------
 import { CURRENT_USER } from "../../graphQL/queries"
 import { useQuery } from "@apollo/react-hooks"
-
-// images & logos ----------------
-import Logo from "../../images/icons/logo.svg"
 
 // components ----------------
 import BurgerButton from "../burger-button/BurgerButton"
@@ -16,24 +13,27 @@ import Backdrop from "../backdrop/Backdrop"
 import QueryError from "../error/Error"
 import Loading from "../loading/Loading"
 
-export const CurrentUserContext = createContext()
+// images & logos ----------------
+import Logo from "../../images/icons/logo.svg"
+
+// apolloClient cache ----------------
+import { client } from "../../apolloClient"
 
 const Navbar = () => {
+  // variables ----------------
+  let backdrop = null
+
   // state ----------------
   const [isSideBarOpen, setSideBar] = useState(false)
 
   // query ----------------
-  const { data, loading, error } = useQuery(CURRENT_USER)
-  let currentUser
+  const { loading, error } = useQuery(CURRENT_USER)
 
   if (loading) return <Loading />
   if (error) return <QueryError errorMessage={error.message} />
-  if (data && data.currentUser) {
-    currentUser = data.currentUser
-  }
 
-  // variables ----------------
-  let backdrop = null
+  // run query in cache
+  const currentUser = client.readQuery({ query: CURRENT_USER }).currentUser
 
   // functions & conditions ----------------
   const handleClickSidebar = () => {
@@ -48,183 +48,180 @@ const Navbar = () => {
 
   // return ----------------
   return (
-    <CurrentUserContext.Provider value={currentUser}>
-      <Router className="navigation">
-        <div className="burger">
-          <div className={isSideBarOpen ? "close-burger" : "open-burger"}>
-            <Link to="/">
-              <img src={Logo} alt="Stans Logo" className="burger__logo" />
-            </Link>
-            <BurgerButton click={handleClickSidebar} />
-          </div>
+    <Router className="navigation">
+      <div className="burger">
+        <div className={isSideBarOpen ? "close-burger" : "open-burger"}>
+          <Link to="/">
+            <img src={Logo} alt="Stans Logo" className="burger__logo" />
+          </Link>
+          <BurgerButton click={handleClickSidebar} />
         </div>
+      </div>
 
-        <nav
-          className={
-            isSideBarOpen ? "show-responsive-nav" : "close-responsive-nav"
-          }
-        >
-          <div className="navigation__items">
-            <div className="navigation__items--logo">
-              <Link to="/">
-                <img src={Logo} alt="Stans Logo" />
-              </Link>
+      <nav
+        className={
+          isSideBarOpen ? "show-responsive-nav" : "close-responsive-nav"
+        }
+      >
+        <div className="navigation__items">
+          <div className="navigation__items--logo">
+            <Link to="/">
+              <img src={Logo} alt="Stans Logo" />
+            </Link>
+          </div>
+
+          <ul className="navigation__items--list">
+            {/* PRIVATE ROUTES */}
+            <div className="menu-top">
+              {/* HOME & DASHBOARD */}
+              {!currentUser ? (
+                <li className="logged-out home">
+                  <NavLink
+                    strict
+                    to="/"
+                    exact
+                    activeClassName="active"
+                    onClick={closeSidebar}
+                  >
+                    Home
+                  </NavLink>
+                </li>
+              ) : (
+                <li className="logged-in dashboard">
+                  <NavLink
+                    strict
+                    to="/"
+                    exact
+                    activeClassName="active"
+                    onClick={closeSidebar}
+                  >
+                    Dashboard
+                  </NavLink>
+                </li>
+              )}
+
+              {/* ABOUT & ADD NEW */}
+              {!currentUser ? (
+                <li className="logged-out about">
+                  <NavLink
+                    strict
+                    to="/about"
+                    exact
+                    activeClassName="active"
+                    onClick={closeSidebar}
+                  >
+                    About
+                  </NavLink>
+                </li>
+              ) : (
+                <li className="logged-in add-new">
+                  <NavLink
+                    strict
+                    to="/add-new"
+                    exact
+                    activeClassName="active"
+                    onClick={closeSidebar}
+                  >
+                    Add New
+                  </NavLink>
+                </li>
+              )}
+
+              {/* LOGIN & Calendar */}
+              {!currentUser ? (
+                <li className="logged-out login">
+                  <NavLink
+                    strict
+                    to="/login"
+                    exact
+                    activeClassName="active"
+                    onClick={closeSidebar}
+                  >
+                    Login
+                  </NavLink>
+                </li>
+              ) : (
+                <li className="logged-in calendar">
+                  <NavLink
+                    strict
+                    to="/calendar"
+                    exact
+                    activeClassName="active"
+                    onClick={closeSidebar}
+                  >
+                    Calendar
+                  </NavLink>
+                </li>
+              )}
+
+              {/* Exams */}
+              {currentUser ? (
+                <li className="logged-in exams">
+                  <NavLink
+                    strict
+                    to="/exams"
+                    exact
+                    activeClassName="active"
+                    onClick={closeSidebar}
+                  >
+                    Exams
+                  </NavLink>
+                </li>
+              ) : null}
+
+              {/* USER PROFILE */}
+              {currentUser ? (
+                <li className="logged-in profile">
+                  <span className="user-avatar">
+                    {currentUser.username.charAt(0)}
+                  </span>
+                  <NavLink
+                    strict
+                    to="/profile"
+                    exact
+                    activeClassName="active"
+                    onClick={closeSidebar}
+                  >
+                    Profile
+                  </NavLink>
+                </li>
+              ) : null}
             </div>
 
-            <ul className="navigation__items--list">
-              {/* PRIVATE ROUTES */}
-              <div className="menu-top">
-                {/* HOME & DASHBOARD */}
-                {!currentUser ? (
-                  <li className="logged-out home">
-                    <NavLink
-                      strict
-                      to="/"
-                      exact
-                      activeClassName="active"
-                      onClick={closeSidebar}
-                    >
-                      Home
-                    </NavLink>
-                  </li>
-                ) : (
-                  <li className="logged-in dashboard">
-                    <NavLink
-                      strict
-                      to="/"
-                      exact
-                      activeClassName="active"
-                      onClick={closeSidebar}
-                    >
-                      Dashboard
-                    </NavLink>
-                  </li>
-                )}
+            {/* PUBLIC ROUTES */}
+            <div className="menu-bottom">
+              <li className="dark-mode">Dark mode</li>
+              <li className="imprint">
+                <NavLink
+                  strict
+                  to="/imprint"
+                  exact
+                  activeClassName="active"
+                  onClick={closeSidebar}
+                >
+                  Imprint
+                </NavLink>
+              </li>
+              <li className="data-policy">
+                <NavLink
+                  strict
+                  to="/data-policy"
+                  exact
+                  activeClassName="active"
+                  onClick={closeSidebar}
+                >
+                  Data Policy
+                </NavLink>
+              </li>
+            </div>
+          </ul>
+        </div>
+      </nav>
 
-                {/* ABOUT & ADD NEW */}
-                {!currentUser ? (
-                  <li className="logged-out about">
-                    <NavLink
-                      strict
-                      to="/about"
-                      exact
-                      activeClassName="active"
-                      onClick={closeSidebar}
-                    >
-                      About
-                    </NavLink>
-                  </li>
-                ) : (
-                  <li className="logged-in add-new">
-                    <NavLink
-                      strict
-                      to="/add-new"
-                      exact
-                      activeClassName="active"
-                      onClick={closeSidebar}
-                    >
-                      Add New
-                    </NavLink>
-                  </li>
-                )}
-
-                {/* LOGIN & Calendar */}
-                {!currentUser ? (
-                  <li className="logged-out login">
-                    <NavLink
-                      strict
-                      to="/login"
-                      exact
-                      activeClassName="active"
-                      onClick={closeSidebar}
-                    >
-                      Login
-                    </NavLink>
-                  </li>
-                ) : (
-                  <li className="logged-in calendar">
-                    <NavLink
-                      strict
-                      to="/calendar"
-                      exact
-                      activeClassName="active"
-                      onClick={closeSidebar}
-                    >
-                      Calendar
-                    </NavLink>
-                  </li>
-                )}
-
-                {/* Exams */}
-                {currentUser ? (
-                  <li className="logged-in exams">
-                    <NavLink
-                      strict
-                      to="/exams"
-                      exact
-                      activeClassName="active"
-                      onClick={closeSidebar}
-                    >
-                      Exams
-                    </NavLink>
-                  </li>
-                ) : null}
-
-                {/* USER PROFILE */}
-                {currentUser ? (
-                  <li className="logged-in profile">
-                    <span className="user-avatar">
-                      {currentUser.username.charAt(0)}
-                    </span>
-                    <NavLink
-                      strict
-                      to="/profile"
-                      exact
-                      activeClassName="active"
-                      onClick={closeSidebar}
-                    >
-                      Profile
-                    </NavLink>
-                  </li>
-                ) : null}
-              </div>
-
-              {/* PUBLIC ROUTES */}
-              <div className="menu-bottom">
-                <li className="dark-mode">Dark mode</li>
-                <li className="imprint">
-                  <NavLink
-                    strict
-                    to="/imprint"
-                    exact
-                    activeClassName="active"
-                    onClick={closeSidebar}
-                  >
-                    Imprint
-                  </NavLink>
-                </li>
-                <li className="data-policy">
-                  <NavLink
-                    strict
-                    to="/data-policy"
-                    exact
-                    activeClassName="active"
-                    onClick={closeSidebar}
-                  >
-                    Data Policy
-                  </NavLink>
-                </li>
-              </div>
-            </ul>
-          </div>
-        </nav>
-
-        {backdrop}
-        <Content />
-      </Router>
-    </CurrentUserContext.Provider>
+      {backdrop}
+      <Content />
+    </Router>
   )
 }
 
 export default Navbar
-export const useCurrentUserValue = () => useContext(CurrentUserContext)
