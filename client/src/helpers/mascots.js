8bb01f93fc9611ab579a8a71fa5c0232f786cd9f
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-05-11 22:02:49 +0200

    add: extract domain function in helper file

--- client/src/helpers/mascots.js
+++ client/src/helpers/mascots.js
@@ -9,3 +9,22 @@ export const currentMood = currentState => {
 
   return mood
 }
+
+export const extractDomain = url => {
+  var domain
+
+  if (url.indexOf("://") > -1) {
+    domain = url.split("/")[2]
+  } else {
+    domain = url.split("/")[0]
+  }
+
+  if (domain.indexOf("www.") > -1) {
+    domain = domain.split("www.")[1]
+  }
+
+  domain = domain.split(":")[0]
+  domain = domain.split("?")[0]
+
+  return domain
+}
