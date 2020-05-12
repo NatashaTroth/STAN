2ddc91c67f7fcdd90c78b0a128bb6b1f34355595
Daniela Kubesch <dkubesch.mmt-b2017@fh-salzburg.ac.at>
2020-05-12 16:19:30 +0200

    adapt: change particle color on home page

--- client/src/components/theme-changer/ThemeChanger.js
+++ client/src/components/theme-changer/ThemeChanger.js
@@ -1,11 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import useDarkMode from "use-dark-mode"
+// --------------------------------------------------------------
 
 const DarkModeToggle = () => {
   const darkMode = useDarkMode(false)
-  if (darkMode) {
-    console.log("yes")
-  }
 
   return (
     <button type="button" onClick={darkMode.toggle} className="dark-mode-btn">
