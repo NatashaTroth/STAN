13409700c4327102f65532e21646c970e49372b0
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-05-13 10:14:03 +0200

    add: error handling and mutation in reset password component

--- client/src/components/reset-password/ResetPassword.js
+++ client/src/components/reset-password/ResetPassword.js
@@ -6,6 +6,10 @@ import { useForm } from "react-hook-form"
 // queries ----------------
 import { CURRENT_USER } from "../../graphQL/queries"
 
+// mutations ----------------
+import { useMutation } from "@apollo/react-hooks"
+import { RESET_PASSWORD_MUTATION } from "../../graphQL/mutations"
+
 // components ----------------
 import Input from "../../components/input/Input"
 import Label from "../../components/label/Label"
@@ -18,6 +22,9 @@ const ResetPassword = () => {
   // form ----------------
   const { register, errors, handleSubmit } = useForm()
 
+  // mutations ----------------
+  const [resetPassword] = useMutation(RESET_PASSWORD_MUTATION)
+
   // redirects ----------------
   const currentUser = client.readQuery({ query: CURRENT_USER }).currentUser
   if (currentUser !== null) {
@@ -26,12 +33,12 @@ const ResetPassword = () => {
 
   // form specific ----------------
   const onSubmit = async formData => {
-    // if (formData.password === formData.retype_password) {
-    //   document.getElementById("signup-error").style.display = "none"
-    //   handleSignup({ formData, signup, notification })
-    // } else {
-    //   document.getElementById("signup-error").style.display = "block"
-    // }
+    if (formData.password === formData.retype_password) {
+      document.getElementById("signup-error").style.display = "none"
+      //   handleResetPassword({ formData, resetPassword })
+    } else {
+      document.getElementById("signup-error").style.display = "block"
+    }
   }
 
   return (
@@ -40,7 +47,7 @@ const ResetPassword = () => {
         <div clasName="row">
           <div className="col-md-1"></div>
           <div className="col-md-10 login__headline">
-            <h2 className="login__headline__heading">Reset password</h2>
+            <h2 className="login__headline__heading">Reset your password</h2>
             <p className="login__headline__sub-heading">bla text bla</p>
           </div>
           <div className="col-md-1"></div>
@@ -48,10 +55,20 @@ const ResetPassword = () => {
           <div className="col-md-2"></div>
           <div className="col-md-8 login__form-container">
             <form onSubmit={handleSubmit(onSubmit)} className="box-content">
+              <div id="signup-error">
+                <div className="error sign-up-error">
+                  <p>Please make sure your passwords match.</p>
+                </div>
+              </div>
+
+              <div className="error-handling-form">
+                <p className="error graphql-sign-up-error"></p>
+              </div>
+
               <div className="login__form__element">
                 <Label
                   for="password"
-                  text="Password"
+                  text="New password"
                   className="login__form__element__label input-required"
                 ></Label>
                 <Input
@@ -86,7 +103,7 @@ const ResetPassword = () => {
               <div className="login__form__element">
                 <Label
                   for="retype_password"
-                  text="Retype password"
+                  text="Confirm password"
                   className="login__form__element__label input-required"
                 ></Label>
                 <Input
@@ -138,3 +155,30 @@ const ResetPassword = () => {
 }
 
 export default ResetPassword
+
+async function handleResetPassword({ formData, resetPassword }) {
+  try {
+    const resp = await resetPassword({
+      variables: {
+        // userId: ,
+        // token: ,
+        // newPassword ,
+      },
+    })
+
+    if (resp && resp.data && resp.data.resetPassword) {
+    } else {
+      throw new Error("Reset password failed")
+    }
+
+    // redirect ----------------
+  } catch (err) {
+    let element = document.getElementsByClassName("graphql-sign-up-error")
+
+    if (err.graphQLErrors && err.graphQLErrors[0]) {
+      element[0].innerHTML = err.graphQLErrors[0].message
+    } else {
+      element[0].innerHTML = err.message
+    }
+  }
+}
