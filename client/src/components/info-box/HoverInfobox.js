e35b3161a3063d432f9e890f275e06b1ac7fa511
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-05-13 17:33:51 +0200

    remove: hover info box component

--- client/src/components/info-box/HoverInfobox.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react"
-// --------------------------------------------------------------
-
-const HoverInfobox = () => {
-  return <div className="infobox"></div>
-}
-
-export default HoverInfobox
