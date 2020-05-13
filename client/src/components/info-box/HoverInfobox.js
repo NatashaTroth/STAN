aa73342a63a40524701aa9517d08f7adeeb1613e
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-05-13 13:07:23 +0200

    add: hover info box component

---
+++ client/src/components/info-box/HoverInfobox.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react"
+// --------------------------------------------------------------
+
+const HoverInfobox = () => {
+  return <div className="infobox"></div>
+}
+
+export default HoverInfobox
