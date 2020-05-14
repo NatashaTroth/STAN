decc08eea7157fd390770d492145b4658544bde7
Daniela Kubesch <dkubesch.mmt-b2017@fh-salzburg.ac.at>
2020-05-14 22:27:30 +0200

    fix: remove useless code from theme changer component

--- client/src/components/theme-changer/ThemeChanger.js
+++ client/src/components/theme-changer/ThemeChanger.js
@@ -5,10 +5,6 @@ import useDarkMode from "use-dark-mode"
 const DarkModeToggle = () => {
   const darkMode = useDarkMode(false)
 
-  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    darkMode.value = true
-  }
-
   return (
     <button type="button" onClick={darkMode.toggle} className="dark-mode-btn">
       {darkMode.value ? "Light Mode" : "Dark Mode"}
