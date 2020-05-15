93a36cac9a09a78d126c2bc056db0ef4663aa47c
Daniela Kubesch <dkubesch.mmt-b2017@fh-salzburg.ac.at>
2020-05-15 13:58:40 +0200

    fix: add dark mode class to html tag for safari dark mode compatibility

--- client/src/components/theme-changer/ThemeChanger.js
+++ client/src/components/theme-changer/ThemeChanger.js
@@ -1,9 +1,11 @@
-import React from "react"
+import React, { initialState } from "react"
 import useDarkMode from "use-dark-mode"
 // --------------------------------------------------------------
 
 const DarkModeToggle = () => {
-  const darkMode = useDarkMode(false)
+  const darkMode = useDarkMode(initialState, {
+    element: document.documentElement,
+  })
 
   return (
     <button type="button" onClick={darkMode.toggle} className="dark-mode-btn">
