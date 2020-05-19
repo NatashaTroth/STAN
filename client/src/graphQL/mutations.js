aa7295d3995e91fce1171567a80df21e76a171bb
Natasha Troth <ntroth.mmt-b2017@fh.salzburg.ac.at>
2020-05-19 21:29:09 +0200

    refactor: sort mutation.js and queries.js

--- client/src/graphQL/mutations.js
+++ client/src/graphQL/mutations.js
@@ -1,8 +1,33 @@
 import { gql } from "apollo-boost" //to make queries
 
+//-----------------USER QUERIES-----------------
+
+export const SIGNUP_MUTATION = gql`
+  mutation(
+    $username: String!
+    $email: String!
+    $password: String
+    $mascot: Int
+    $allowEmailNotifications: Boolean!
+  ) {
+    signup(
+      username: $username
+      email: $email
+      password: $password
+      mascot: $mascot
+      allowEmailNotifications: $allowEmailNotifications
+    )
+  }
+`
 export const LOGIN_MUTATION = gql`
   mutation($email: String!, $password: String) {
     login(email: $email, password: $password)
+  }
+`
+
+export const GOOGLE_LOGIN_MUTATION = gql`
+  mutation($idToken: String!) {
+    googleLogin(idToken: $idToken)
   }
 `
 
@@ -11,8 +36,57 @@ export const LOGOUT_MUTATION = gql`
     logout
   }
 `
+export const FORGOTTEN_PASSWORD_EMAIL = gql`
+  mutation($email: String!) {
+    forgottenPasswordEmail(email: $email)
+  }
+`
 
-//!!Make sure the type in mutation here, is the same type as used by graphql
+export const RESET_PASSWORD_MUTATION = gql`
+  mutation($userId: String!, $token: String!, $newPassword: String!) {
+    resetPassword(userId: $userId, token: $token, newPassword: $newPassword)
+  }
+`
+export const UPDATE_MASCOT_MUTATION = gql`
+  mutation($mascot: Int!) {
+    updateMascot(mascot: $mascot)
+  }
+`
+
+export const UPDATE_USER_MUTATION = gql`
+  mutation(
+    $username: String!
+    $email: String!
+    $password: String
+    $newPassword: String
+    $mascot: Int!
+    $allowEmailNotifications: Boolean!
+  ) {
+    updateUser(
+      username: $username
+      email: $email
+      password: $password
+      newPassword: $newPassword
+      mascot: $mascot
+      allowEmailNotifications: $allowEmailNotifications
+    ) {
+      id
+      username
+      email
+      mascot
+      googleLogin
+      allowEmailNotifications
+    }
+  }
+`
+
+export const DELETE_USER_MUTATION = gql`
+  mutation {
+    deleteUser
+  }
+`
+
+//-----------------EXAM QUERIES-----------------
 export const ADD_EXAM_MUTATION = gql`
   mutation(
     $subject: String!
@@ -99,79 +173,5 @@ export const DELETE_EXAM_MUTATION = gql`
 export const UPDATE_CURRENT_PAGE_MUTATION = gql`
   mutation($examId: ID!, $page: Int!) {
     updateCurrentPage(examId: $examId, page: $page)
-  }
-`
-
-export const SIGNUP_MUTATION = gql`
-  mutation(
-    $username: String!
-    $email: String!
-    $password: String
-    $mascot: Int
-    $allowEmailNotifications: Boolean!
-  ) {
-    signup(
-      username: $username
-      email: $email
-      password: $password
-      mascot: $mascot
-      allowEmailNotifications: $allowEmailNotifications
-    )
-  }
-`
-
-export const UPDATE_MASCOT_MUTATION = gql`
-  mutation($mascot: Int!) {
-    updateMascot(mascot: $mascot)
-  }
-`
-
-export const GOOGLE_LOGIN_MUTATION = gql`
-  mutation($idToken: String!) {
-    googleLogin(idToken: $idToken)
-  }
-`
-
-export const UPDATE_USER_MUTATION = gql`
-  mutation(
-    $username: String!
-    $email: String!
-    $password: String
-    $newPassword: String
-    $mascot: Int!
-    $allowEmailNotifications: Boolean!
-  ) {
-    updateUser(
-      username: $username
-      email: $email
-      password: $password
-      newPassword: $newPassword
-      mascot: $mascot
-      allowEmailNotifications: $allowEmailNotifications
-    ) {
-      id
-      username
-      email
-      mascot
-      googleLogin
-      allowEmailNotifications
-    }
-  }
-`
-
-export const DELETE_USER_MUTATION = gql`
-  mutation {
-    deleteUser
-  }
-`
-export const FORGOTTEN_PASSWORD_EMAIL = gql`
-  mutation($email: String!) {
-    forgottenPasswordEmail(email: $email)
-  }
-`
-
-export const RESET_PASSWORD_MUTATION = gql`
-  mutation($userId: String!, $token: String!, $newPassword: String!) {
-    resetPassword(userId: $userId, token: $token, newPassword: $newPassword)
   }
 `
--- server/tests/mutations.js
+++ server/tests/mutations.js
@@ -1,8 +1,32 @@
 import { gql } from "apollo-boost"; //to make queries
 
+//-----------------USER QUERIES-----------------
+export const SIGNUP_MUTATION = gql`
+  mutation(
+    $username: String!
+    $email: String!
+    $password: String
+    $mascot: Int
+    $allowEmailNotifications: Boolean!
+  ) {
+    signup(
+      username: $username
+      email: $email
+      password: $password
+      mascot: $mascot
+      allowEmailNotifications: $allowEmailNotifications
+    )
+  }
+`;
 export const LOGIN_MUTATION = gql`
   mutation($email: String!, $password: String) {
     login(email: $email, password: $password)
+  }
+`;
+
+export const GOOGLE_LOGIN_MUTATION = gql`
+  mutation($idToken: String!) {
+    googleLogin(idToken: $idToken)
   }
 `;
 
@@ -12,7 +36,58 @@ export const LOGOUT_MUTATION = gql`
   }
 `;
 
-//!!Make sure the type in mutation here, is the same type as used by graphql
+export const RESET_PASSWORD_MUTATION = gql`
+  mutation($userId: String!, $token: String!, $newPassword: String!) {
+    resetPassword(userId: $userId, token: $token, newPassword: $newPassword)
+  }
+`;
+
+export const FORGOTTEN_PASSWORD_EMAIL = gql`
+  mutation($email: String!) {
+    forgottenPasswordEmail(email: $email)
+  }
+`;
+
+export const UPDATE_MASCOT_MUTATION = gql`
+  mutation($mascot: Int!) {
+    updateMascot(mascot: $mascot)
+  }
+`;
+
+export const UPDATE_USER_MUTATION = gql`
+  mutation(
+    $username: String!
+    $email: String!
+    $password: String
+    $newPassword: String
+    $mascot: Int!
+    $allowEmailNotifications: Boolean!
+  ) {
+    updateUser(
+      username: $username
+      email: $email
+      password: $password
+      newPassword: $newPassword
+      mascot: $mascot
+      allowEmailNotifications: $allowEmailNotifications
+    ) {
+      id
+      username
+      email
+      mascot
+      googleLogin
+      allowEmailNotifications
+    }
+  }
+`;
+
+export const DELETE_USER_MUTATION = gql`
+  mutation {
+    deleteUser
+  }
+`;
+
+//-----------------EXAM QUERIES-----------------
 export const ADD_EXAM_MUTATION = gql`
   mutation(
     $subject: String!
@@ -96,83 +171,8 @@ export const UPDATE_CURRENT_PAGE_MUTATION = gql`
   }
 `;
 
-export const SIGNUP_MUTATION = gql`
-  mutation(
-    $username: String!
-    $email: String!
-    $password: String
-    $mascot: Int
-    $allowEmailNotifications: Boolean!
-  ) {
-    signup(
-      username: $username
-      email: $email
-      password: $password
-      mascot: $mascot
-      allowEmailNotifications: $allowEmailNotifications
-    )
-  }
-`;
-
-export const UPDATE_MASCOT_MUTATION = gql`
-  mutation($mascot: Int!) {
-    updateMascot(mascot: $mascot)
-  }
-`;
-
-export const GOOGLE_LOGIN_MUTATION = gql`
-  mutation($idToken: String!) {
-    googleLogin(idToken: $idToken)
-  }
-`;
-
-export const UPDATE_USER_MUTATION = gql`
-  mutation(
-    $username: String!
-    $email: String!
-    $password: String
-    $newPassword: String
-    $mascot: Int!
-    $allowEmailNotifications: Boolean!
-  ) {
-    updateUser(
-      username: $username
-      email: $email
-      password: $password
-      newPassword: $newPassword
-      mascot: $mascot
-      allowEmailNotifications: $allowEmailNotifications
-    ) {
-      id
-      username
-      email
-      mascot
-      googleLogin
-      allowEmailNotifications
-    }
-  }
-`;
-
-export const DELETE_USER_MUTATION = gql`
-  mutation {
-    deleteUser
-  }
-`;
-
 export const EXAM_COMPLETED_MUTATION = gql`
   mutation($id: ID!, $completed: Boolean!) {
     examCompleted(id: $id, completed: $completed)
-  }
-`;
-
-export const RESET_PASSWORD_MUTATION = gql`
-  mutation($userId: String!, $token: String!, $newPassword: String!) {
-    resetPassword(userId: $userId, token: $token, newPassword: $newPassword)
-  }
-`;
-
-export const FORGOTTEN_PASSWORD_EMAIL = gql`
-  mutation($email: String!) {
-    forgottenPasswordEmail(email: $email)
   }
 `;
