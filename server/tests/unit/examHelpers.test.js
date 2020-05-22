1dd936a72a8ab0a67162b0f434bcccf2e4bd83cd
Natasha Troth <ntroth.mmt-b2017@fh.salzburg.ac.at>
2020-05-22 17:43:48 +0200

    fix: do not create chunks of exams where exam date is today

---
+++ server/tests/unit/examHelpers.test.js
@@ -0,0 +1,8 @@
+import { learningIsComplete } from "../../helpers/exams/examHelpers";
+
+// export function learningIsComplete(currentPage, startPage, numberPages, repeat = 1) {
+test("that the float is rounded to two decimals correctly", () => {
+  expect(learningIsComplete(101, 1, 100)).toBeTruthy();
+  expect(learningIsComplete(201, 1, 100, 2)).toBeTruthy();
+  expect(learningIsComplete(50, 20, 100)).toBeFalsy();
+});
