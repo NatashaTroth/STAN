5349033388c71db75f51a0e621bff1ac2c06fc9f
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-05-23 14:27:36 +0200

    uncomment: particles library in home

--- client/src/pages/calendar-page/CalendarPage.js
+++ client/src/pages/calendar-page/CalendarPage.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react"
+import React from "react"
 import { Redirect } from "react-router-dom"
 import ReactDOM from "react-dom"
 // --------------------------------------------------------------
