f246a39fc5c81f3da28dcbdbab78fb575d8a4039
Tra Nguyen <hnguyen.mmt-b2017@fh-salzburg.ac.at>
2020-05-24 17:22:41 +0200

    remove: unused  and update all outdated npm packages

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,13 +1,44 @@
-const purgecss = require("@fullhuman/postcss-purgecss")
+const path = require("path")
+const glob = require("glob")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const PurgecssPlugin = require("purgecss-webpack-plugin")
+
+const PATHS = {
+  src: path.join(__dirname, "src"),
+}
 
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [
-        purgecss({
-          content: ["./public/**/*.html"],
-        }),
-      ],
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          test: /\.css$/,
+          chunks: "all",
+          enforce: true,
+        },
+      },
     },
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+    }),
+  ],
 }
